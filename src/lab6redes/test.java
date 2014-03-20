71d21cc6cfbfe6be308ed6c097e69ab3992fabd2
Camilo Barraza <ca.barraza961@uniandes.edu.co>
2014-03-20 19:44:51 -0400

    cotole

--- src/lab6redes/test.java
+++ src/lab6redes/test.java
@@ -2,10 +2,7 @@ package lab6redes;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println( "coletisimo");
-		System.out.println( "cambio ");
-		System.out.println("otro meque");
-		System.out.println("otro otro meque");
+		System.out.println( "coletisimo2");
 	}
 
 }
